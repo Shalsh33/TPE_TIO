fa876c251702242715b7de116357c888349d6d28
Blas Martínez <blas.e.martinez@live.com>
2020-10-03 23:27:01 -0300

    avances sobre el controller del login

---
+++ app/controller/login.controller.php
@@ -0,0 +1,36 @@
+<?php
+
+class login_controller {
+	
+	private $model;
+	private $view;
+	
+	function __construct(){
+		
+		$this->model = new users_model();
+		$this->view = new login_view();
+		
+	}
+	
+	function login_form(){
+		
+		$this->view->login_form();
+		
+	}
+	
+	function login() {
+		
+		$name = $_POST['name'];
+		$pass = $_POST['pass'];
+		
+		$db_pass = $this->model->get($name);
+		
+		if (password_verify($pass,$db_pass)){
+			$this->view->admin();
+		} else {
+			$this->view->error();
+		}
+		
+	}	
+	
+}
